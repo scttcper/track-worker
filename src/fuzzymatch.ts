3379471adccdca6704af5ec040ca2cc68cb740f4
Scott Cooper <scttcper@gmail.com>
2024-03-24 20:32:48 -0700

    clean strings?

--- src/fuzzymatch.ts
+++ src/fuzzymatch.ts
@@ -47,17 +47,19 @@ const cleanString = (str: string) =>
  * Album and release date are optional
  */
 export function fuzzymatchSong(inputSong: InputSong, songList: ResultSong[]) {
-  const trackSet = fuzzySet(songList.map(x => x.title));
-  const titleScores = trackSet.get(inputSong.title);
-  const artistSet = fuzzySet(songList.map(x => x.artists));
-  const artistScores = artistSet.get(inputSong.artists);
+  const trackSet = fuzzySet(songList.map(x => cleanString(x.title)));
+  const titleScores = trackSet.get(cleanString(inputSong.title));
+  const artistSet = fuzzySet(songList.map(x => cleanString(x.artists)));
+  const artistScores = artistSet.get(cleanString(inputSong.artists));
   const albumSet = inputSong.album ? fuzzySet(songList.map(x => x.album ?? '')) : null;
   const albumScores = inputSong.album ? albumSet?.get(inputSong.album ?? '') : null;
 
   return songList
     .map(song => {
-      const titleScore = titleScores?.find(score => score[1] === song.title)?.[0] ?? -1;
-      const artistScore = artistScores?.find(score => score[1] === song.artists)?.[0] ?? -1;
+      const titleScore =
+        titleScores?.find(score => score[1] === cleanString(song.title))?.[0] ?? -1;
+      const artistScore =
+        artistScores?.find(score => score[1] === cleanString(song.artists))?.[0] ?? -1;
       const albumScore = albumScores?.find(score => score[1] === (song.album ?? ''))?.[0] ?? 0;
 
       console.log({
